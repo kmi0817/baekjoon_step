77f72fe15bcb8ae40bf8f43f03c33bf64c806575
박경미 <62174395+kmi0817@users.noreply.github.com>
2021-12-28 10:17:04 +0900

    change the way of using input()

--- step1/10.py
+++ step1/10.py
@@ -1,8 +1,8 @@
 try :
-    abc_list = input().split(" ")
-    A = int(abc_list[0])
-    B = int(abc_list[1])
-    C = int(abc_list[2])
+    a, b, c = input().split()
+    A = int(a)
+    B = int(b)
+    C = int(c)
 
     # if A >= 2 and A <=10000 and B >= 2 and B <=10000 and C >= 2 and C <=10000 :
     if A in range(2, 10001) and B in range(2, 10001) and C in range(2, 10001):
@@ -10,8 +10,23 @@ try :
         print(((A%C) + (B%C)) % C)
         print((A*B)%C)
         print(((A%C) * (B%C)) % C)
-    else :
-        print("범위가 올바르지 않습니다.")
-
 except :
-    print("정수입력")
+    print("정수를 입력하시오")
+
+# try :
+#     abc_list = input().split(" ")
+#     A = int(abc_list[0])
+#     B = int(abc_list[1])
+#     C = int(abc_list[2])
+
+#     # if A >= 2 and A <=10000 and B >= 2 and B <=10000 and C >= 2 and C <=10000 :
+#     if A in range(2, 10001) and B in range(2, 10001) and C in range(2, 10001):
+#         print((A+B)%C)
+#         print(((A%C) + (B%C)) % C)
+#         print((A*B)%C)
+#         print(((A%C) * (B%C)) % C)
+#     else :
+#         print("범위가 올바르지 않습니다.")
+
+# except :
+#     print("정수입력")
--- step1/9.py
+++ step1/9.py
@@ -1,7 +1,7 @@
 try :
-    ab_list = input().split(" ")
-    A = int(ab_list[0])
-    B = int(ab_list[1])
+    a, b = input().split()
+    A = int(a)
+    B = int(b)
 
     if A >= 1 and B <= 10000 :
         print(A+B)
@@ -9,7 +9,23 @@ try :
         print(A*B)
         print(A//B)
         print(A%B)
-except ZeroDivisionError :
+except ZeroDivisionError:
     print("0으로 나눌 수 없습니다.")
 except :
-    print("정수를 입력하세요.")
+    print("정수를 입력하시오")
+
+# try :
+#     ab_list = input().split(" ")
+#     A = int(ab_list[0])
+#     B = int(ab_list[1])
+
+#     if A >= 1 and B <= 10000 :
+#         print(A+B)
+#         print(A-B)
+#         print(A*B)
+#         print(A//B)
+#         print(A%B)
+# except ZeroDivisionError :
+#     print("0으로 나눌 수 없습니다.")
+# except :
+#     print("정수를 입력하세요.")
