42fd47a6ea9fcebe359a10bbd8595825f70973d4
박경미 <62174395+kmi0817@users.noreply.github.com>
2021-12-28 15:08:24 +0900

    wrong...
    I don't know why

--- step2/5.py
+++ step2/5.py
@@ -2,19 +2,14 @@ try :
     h, m = input().split()
     H = int(h)
     M = int(m)
-
-    if H not in range(0, 24) or M not in range(0, 60) :
-        exit()
-    
-    # 이 코드는 자꾸 except으로 넘어감. 왜?...
-    # if H == 0 and M < 45 :
-    #     print("23 " + abs(M-45))
-    if M < 45 :
-        if H-1 < 0 :
-            print(24 + (H-1), 60 - (45-M))
-        else :
-            print(H-1, 60 - (45-M))
+    if M > 45 and M < 59 :
+        print(H, M-45)
+    elif M == 45 :
+        print(H, 0)
     else :
-        print(H, 60 - (45 - M))
+        if H == 0 :
+            H = 24
+        print(H-1, 60+(M-45))
+
 except :
-    print("입력이 잘못되었습니다")
+    print("입력이 잘못되었습니다.")
