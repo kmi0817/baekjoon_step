356c0ff27c0a99cbcce5fac1ee2096fcb85cddfd
박경미 <62174395+kmi0817@users.noreply.github.com>
2021-12-28 13:46:28 +0900

    wrong
    예외 케이스: 12 45 입력 => 12 60 출력

--- step2/5.py
+++ step2/5.py
@@ -6,11 +6,15 @@ try :
     if H not in range(0, 24) or M not in range(0, 60) :
         exit()
     
-    if H == 0 and M < 45 :
-        print("23 " + abs(M-45))
-    elif M < 45 :
-        print(H-1, abs(M-45))
+    # 이 코드는 자꾸 except으로 넘어감. 왜?...
+    # if H == 0 and M < 45 :
+    #     print("23 " + abs(M-45))
+    if M < 45 :
+        if H-1 < 0 :
+            print(24 + (H-1), 60 - (45-M))
+        else :
+            print(H-1, 60 - (45-M))
     else :
-        print(H, M-45)
+        print(H, 60 - (45 - M))
 except :
     print("입력이 잘못되었습니다")
