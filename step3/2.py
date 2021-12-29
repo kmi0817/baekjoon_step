946025acebd1ca934f1327268e3d54f9f1a24ef6
박경미 <62174395+kmi0817@users.noreply.github.com>
2021-12-29 13:49:16 +0900

    add - A, B range check

--- step3/2.py
+++ step3/2.py
@@ -4,8 +4,16 @@ try :
     ab_list = list()
     for i in range(cnt) :
         temp_a, temp_b = input().split()
-        ab_list.append(int(temp_a))
-        ab_list.append(int(temp_b))
+
+        a = int(temp_a)
+        b = int(temp_b)
+
+        if a > 0 and b < 10 :
+            ab_list.append(int(temp_a))
+            ab_list.append(int(temp_b))
+        else :
+            print("수의 범위가 올바르지 않습니다.")
+            break
 
     for i in range(0, cnt*2, 2) :
         print(ab_list[i] + ab_list[i+1])
