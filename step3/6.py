c1ec9bd60ce555037757ced7b78c4481a2cc1830
박경미 <62174395+kmi0817@users.noreply.github.com>
2021-12-29 14:48:13 +0900

    Add files via upload

---
+++ step3/6.py
@@ -0,0 +1,10 @@
+try :
+    N = int(input())
+
+    if N >= 1 and N <= 100000 :
+        for i in range(N) :
+            print(N-i)
+    else :
+        print("N은 100,000 이하의 자연수입니다.")
+except :
+    print("입력이 잘못되었습니다.")
