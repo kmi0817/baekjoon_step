2f80b593f125ac89ab4a379ce9e5c5cad2f7782b
kmi0817 <kmi0817@naver.com>
2021-12-30 19:26:15 +0900

    add another way to solve

--- step5/3.py
+++ step5/3.py
@@ -1,3 +1,26 @@
+# 방법1) count() 직접 구현
+def my_count(res) :
+    count_list = list()
+
+    for i in range(10) :
+        cnt = 0
+        for c in res :
+            if str(i) == c :
+                cnt += 1
+        count_list.append(cnt)
+
+    return count_list
+
+A = int(input())
+B = int(input())
+C = int(input())
+
+result = A * B * C
+cnt_list = my_count(str(result))
+for e in cnt_list :
+    print(e)
+
+# 방법2) count() 내장 함수 사용
 A = int(input())
 B = int(input())
 C = int(input())
