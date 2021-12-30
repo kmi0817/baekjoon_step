d2a8aeae976c9e0daf45b494df1e064435343023
kmi0817 <kmi0817@naver.com>
2021-12-31 08:50:53 +0900

    add comments

--- step5/5.py
+++ step5/5.py
@@ -1,3 +1,4 @@
+# 점수/M*100 만드는 함수 (M: 최대값)
 def make_newScore(m, score_list) :
     new_score_list = list()
 
@@ -6,30 +7,24 @@ def make_newScore(m, score_list) :
 
     return new_score_list
 
+# 평균 구하는 함수
 def make_avg(score_list) :
     return sum(score_list)/len(score_list)
 
+
+# 과목 개수 입력
 N = int(input())
 if N > 1000 or N < 0 :
     exit()
 
+# 과목별 점수 입력
 original_score_list = list(map(int, input().split()))
 if len(original_score_list) != N :
     exit()
 
-
-# # 100보다 작거나 같은 수
-# if a > 100 or b > 100 or c > 100 :
-#     exit()
-# # 음수 아닌 정수
-# elif a < 0 or b < 0 or c < 0 :
-#     exit()
-# # 적어도 하나의 값은 0보다 크다
-# elif a == 0 and b == 0 and c == 0 :
-#     exit()
-
-# abc_list = [a, b, c]
+# 과목 내 최대값 구하기
 M = max(original_score_list)
 
+# 함수 적용 및 출력
 new_score_list = make_newScore(M, original_score_list)
 print(make_avg(new_score_list))
