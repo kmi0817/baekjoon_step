0fa7337134e6ecf7a4b6314c4539c1ea658f6970
박경미 <62174395+kmi0817@users.noreply.github.com>
2022-01-01 12:12:58 +0900

    problem solved
    input() 받은 다음 len(S) 바로 체크하는 것이 옳바르지 않았던 것 같음.
    띄어쓰기 하나 입력하여도 len(S)는 1이 되니까... 사실 잘 모르겠다.

--- step7/6.py
+++ step7/6.py
@@ -1,6 +1,10 @@
-S = input().strip()
-if len(S) > 1000000 or len(S) < 1 :
-    exit()
+S = input().strip().split()
+print(len(S))
 
-S_list = S.split()
-print(len(S_list))
+
+# S = input().strip()
+# if len(S) > 1000000 or len(S) < 1 :
+#     exit()
+
+# S_list = S.split()
+# print(len(S_list))
