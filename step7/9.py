e85004f23e137f199cd3d9468dd2e47912aa9099
kmi0817 <kmi0817@naver.com>
2022-01-02 10:15:36 +0900

    done

--- step7/9.py
+++ step7/9.py
@@ -1,16 +1,18 @@
 croatia_alphabet = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
 check_alphabet = ["c", "d", "l", "n", "s", "z"] # z, j 제외
 
+###
 S = input()
 
 total_length = len(S)
 special_cnt = 0
-for index, c in enumerate(S) :
-    if c in check_alphabet :
-        if c == "d" and S[index+1] == "z" :
-            test_alphabet = 
-        c_and_next = S[index:]
+for i in range(len(S)) :
+    if S[i] in check_alphabet :
+        char_2 = S[i:i+2]
+        char_3 = S[i:i+3]
 
-        if c_and_next in croatia_alphabet :
+        if char_3 == "dz=" :
+            special_cnt +=1
+        elif char_2 in croatia_alphabet :
             special_cnt += 1
-print(len(S) - special_cnt)
+print(total_length - special_cnt)
