a38ca168fed6c09766903fdfd751c36a93220464
kmi0817 <kmi0817@naver.com>
2022-01-02 11:55:26 +0900

    step7- 10

--- step7/10.py
+++ step7/10.py
@@ -1,26 +1,23 @@
+import sys
 N = int(input())
 
-exit_char = list()
-
-former_char = ""
-is_group_word = True
 group_word_cnt = 0
 for i in range(N) :
-    word = input().lower()
-
-    for char in word :
-        if char == former_char :
-            is_group_word = False
-            break
-        elif char in exit_char :
-            is_group_word = False
-            break
-        else :
-            exit_char.append(char)
-
+    exit_chars = list()
     former_char = ""
     is_group_word = True
-    exit_char.clear()
+
+    word = sys.stdin.readline().lower()
+
+    for char in word :
+        if char != former_char and char in exit_chars :
+            is_group_word = False
+            break
+        elif char != former_char and char not in exit_chars :
+            exit_chars.append(char)
+
+        former_char = char
+    
     if is_group_word :
         group_word_cnt += 1
 
