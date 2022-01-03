1966c7f78756ee33a0848ef42c662c457c44f39c
kmi0817 <kmi0817@naver.com>
2022-01-03 13:55:04 +0900

    done

--- step8/1.py
+++ step8/1.py
@@ -1,5 +1,6 @@
 A, B, C = map(int, input().split())
 
+# 입력 범위 제한
 if A < 1 or A > 2100000000 :
     print("A is between 1 ~ 2100000000")
     exit()
@@ -8,13 +9,10 @@ elif B < 1 or B > 2100000000 :
     exit()
 elif C < 1 or C > 2100000000 :
     print("C is between 1 ~ 2100000000")
+    exit()
 
-sales_price = C
-production_price = A + (B * 10)
-break_even_point = 1
 
-while sales_price <= production_price :
-    sales_price += C
-    break_even_point += 1
-
-print(break_even_point)
+if C <= B :
+    print(-1)
+else :
+    print(A // (C-B) + 1)
