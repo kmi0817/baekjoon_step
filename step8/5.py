5be47c1bcb92a0b2299bb179942dbd8bc0b9c5cb
kmi0817 <kmi0817@naver.com>
2022-01-05 13:38:04 +0900

    wrong : test case 6 12 6 ==> 002

---
+++ step8/5.py
@@ -0,0 +1,16 @@
+test_case = int(input())
+for i in range(test_case) :
+    h, w, n = map(int, input().split())
+
+    if h < 1 or w > 99 or n < 1 or h * w < n :
+        print("Check range")
+        exit()
+
+    floor = n % h
+    room = n // h + 1
+
+    if room < 10 :
+        print(f'{floor}0{room}')
+        
+    else :
+        print(f'{floor}{room}')
