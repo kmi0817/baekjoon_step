7f40dd26b49f44c4b5c0a3df62c8d7b9205e924f
kmi0817 <kmi0817@naver.com>
2022-01-06 11:35:32 +0900

    add return instead of print()

--- leetcode/125_valid_palindrome.py
+++ leetcode/125_valid_palindrome.py
@@ -1,19 +1,22 @@
-s = input().lower() # make lower
-s_list = []
-for char in s:
-    if char.isalnum() :
-        s_list.append(char)
-print(s_list)
-is_palindrome = True
-while True :
-    try :
-        left_char = s_list.pop(0)
-        right_char = s_list.pop()
-    except IndexError :
-        break
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
 
-    if left_char != right_char :
-        is_palindrome = False
-        break
+        s_list = []
+        for char in s:
+            if char.isalnum() :
+                s_list.append(char)
 
-print(is_palindrome)
+        is_palindrome = True
+        while True :
+            try :
+                left_char = s_list.pop(0)
+                right_char = s_list.pop()
+            except IndexError :
+                break
+
+            if left_char != right_char :
+                is_palindrome = False
+                break
+                
+        return is_palindrome
