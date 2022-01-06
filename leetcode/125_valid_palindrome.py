055a7381d5a4bdbbbcb1e7fd287c8da054dadfd9
kmi0817 <kmi0817@naver.com>
2022-01-06 13:27:40 +0900

    two different ways

--- leetcode/125_valid_palindrome.py
+++ leetcode/125_valid_palindrome.py
@@ -1,3 +1,15 @@
+# 1) use string slicing (runtime: 40ms, memory: 15.2MB)
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s_list = []
+        for char in s.lower():
+            if char.isalnum() :
+                s_list.append(char)
+
+        updated_s = "".join(s_list)
+        return updated_s == updated_s[::-1]
+
+# 2) use list (runtime: 268ms, memory: 14.9MB)
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
@@ -8,9 +20,7 @@ class Solution:
                 s_list.append(char)
 
         is_palindrome = True
-        while len(s_list) > 1 :
-            if s_list.pop(0) != s_list.pop() :
-                return False
+        while True :
             try :
                 left_char = s_list.pop(0)
                 right_char = s_list.pop()
@@ -22,3 +32,17 @@ class Solution:
                 break
                 
         return is_palindrome
+
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         s_list = []
+#         for char in s:
+#             if char.isalnum() :
+#                 s_list.append(char.lower())
+
+#         is_palindrome = True
+#         while len(s_list) > 1 :
+#             if s_list.pop(0) != s_list.pop() :
+#                 is_palindrome = False
+#                 break
+#         return is_palindrome
