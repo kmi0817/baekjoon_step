35eaaad20f1969a8b0d32b335f74c60feaba6d5a
kmi0817 <kmi0817@naver.com>
2022-01-06 15:23:44 +0900

    use 정규식

--- leetcode/819_most_common_word.py
+++ leetcode/819_most_common_word.py
@@ -1,3 +1,18 @@
+# accepted (runtime: 52ms, memory: 14.3MB)
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        # 1) remove character that is not alphanumeric 2) make it lower 3) make list
+        paragraph_list = re.sub(f'[^\w]', ' ', paragraph).lower().split()
+        
+        # filter the banned words
+        words_except_banned = [word for word in paragraph_list if word not in banned]
+        
+        # count each word
+        counter_dict = collections.Counter(words_except_banned)
+        
+        return counter_dict.most_common(1)[0][0]
+
+# wrong answer (before using collection.Counter(), the problem occured)
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         updated_paragraph = ""
