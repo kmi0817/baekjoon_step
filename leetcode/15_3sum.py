ae7a4497d063f0821b25efab8367e4c8fe8fcc19
kmi0817 <kmi0817@naver.com>
2022-01-10 10:42:37 +0900

    accepted (two pointer)

--- leetcode/15_3sum.py
+++ leetcode/15_3sum.py
@@ -2,13 +2,23 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         if not nums or len(nums) < 3 :
             return []
-
+        
+        nums.sort()
         ret_list = []
-        for i, a in enumerate(nums) :
-            for j, b in enumerate(nums[i+1:]) :
-                temp_c = 0 - (a+b)
-                if temp_c in nums[i+j+2:] :
-                    triplet = sorted([a, b, temp_c])
-                    if triplet not in ret_list :
-                        ret_list.append(triplet)
+        for i in range(len(nums) - 2) :
+            if i > 0 and nums[i] == nums[i-1] :
+                continue
+
+            left, right = i + 1, len(nums) - 1
+            while left < right :
+                temp_sum = nums[i] + nums[left] + nums[right]
+                if temp_sum > 0 :
+                    right -= 1
+                elif temp_sum < 0 :
+                    left += 1
+                else :
+                    if [nums[i], nums[left], nums[right]] not in ret_list :
+                        ret_list.append([nums[i], nums[left], nums[right]])
+                    right -= 1
+                    left += 1
         return ret_list
