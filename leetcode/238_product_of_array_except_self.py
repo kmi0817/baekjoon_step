8ca675afaa6ec2a5a619c0fd73fca3a93ef6735a
kmi0817 <kmi0817@naver.com>
2022-01-12 10:06:38 +0900

    accepted (runtime: 429ms, memory: 21.3MB)

--- leetcode/238_product_of_array_except_self.py
+++ leetcode/238_product_of_array_except_self.py
@@ -1,15 +1,25 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        product_all = 1
-        for n in nums :
-            if n != 0 :
-                product_all *= n
+        zero_cnt = nums.count(0)
+        
+        if zero_cnt == 1 :
+            product_except_self = [0 for i in range(len(nums))]
+            index_of_zero = nums.index(0)
+            product_result = 1
+            for n in (nums[0:index_of_zero] + nums[index_of_zero + 1:]) :
+                product_result *= n
+            product_except_self[index_of_zero] = product_result
+            return product_except_self
             
-        product_except_self = []
-        for n in nums :
-            if n != 0 :
-                product_except_self.append(product_all//n)
-            else :
-                product_except_self.append(product_all)
+        elif zero_cnt > 1 :
+            return [0 for x in range(len(nums))]
+        
+        else :
+            product_result = 1
+            for n in nums :
+                product_result *= n
             
-        return product_except_self
+            product_except_self = []
+            for n in nums :
+                product_except_self.append(product_result // n)
+            return product_except_self
