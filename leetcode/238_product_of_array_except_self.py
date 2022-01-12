a00c561c7a6364ff9da8c5d3bca98ca6b1325d0e
kmi0817 <kmi0817@naver.com>
2022-01-12 09:48:12 +0900

    wrong answer (in: -1 1 0 -3 3 / out: -9 9 9 -3 -3)

--- leetcode/238_product_of_array_except_self.py
+++ leetcode/238_product_of_array_except_self.py
@@ -1,7 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        product_list = []
-        for i in range(len(nums)) :
-            array_except_i = list(map(str, nums[0:i] + nums[i+1:]))
-            product_list.append(eval('*'.join(array_except_i)))
-        return product_list
+        product_all = 1
+        for n in nums :
+            if n != 0 :
+                product_all *= n
+            
+        product_except_self = []
+        for n in nums :
+            if n != 0 :
+                product_except_self.append(product_all//n)
+            else :
+                product_except_self.append(product_all)
+            
+        return product_except_self
