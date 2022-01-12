bcd98a7484b6cd56197091a7724b5cfafc763daa
kmi0817 <kmi0817@naver.com>
2022-01-12 10:17:11 +0900

    enhanced runtime by using 0 in nums (234ms)

--- leetcode/238_product_of_array_except_self.py
+++ leetcode/238_product_of_array_except_self.py
@@ -1,25 +1,23 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        zero_cnt = nums.count(0)
-        
-        if zero_cnt == 1 :
-            product_except_self = [0 for i in range(len(nums))]
-            index_of_zero = nums.index(0)
-            product_result = 1
-            for n in (nums[0:index_of_zero] + nums[index_of_zero + 1:]) :
-                product_result *= n
-            product_except_self[index_of_zero] = product_result
-            return product_except_self
+        if 0 in nums :
+            if nums.count(0) > 1 :
+                return [0 for i in range(len(nums))]
+            else : # nums.count(0) == 1
+                result = [0 for i in range(len(nums))]
+                
+                index_of_zero = nums.index(0)
+                product = 1
+                for n in nums[:index_of_zero] + nums[index_of_zero + 1:] :
+                    product *= n
+                result[index_of_zero] = product
+                return result
             
-        elif zero_cnt > 1 :
-            return [0 for x in range(len(nums))]
-        
         else :
-            product_result = 1
+            result = []
+            product = 1
             for n in nums :
-                product_result *= n
-            
-            product_except_self = []
+                product *= n
             for n in nums :
-                product_except_self.append(product_result // n)
-            return product_except_self
+                result.append(product // n)
+            return result
