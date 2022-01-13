d3ca21e01ed7092f01372e82c74e7a20d9a8307a
kmi0817 <kmi0817@naver.com>
2022-01-13 09:56:43 +0900

    time limit exceeded

--- leetcode/ 121_best_time_to_buy_and_sell_stock.py
+++ leetcode/ 121_best_time_to_buy_and_sell_stock.py
@@ -1,10 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         max_profit = 0
-        for i in range(len(prices) - 1) :
-            profit = prices[i+1] - prices[i]
+        for i, price_i in enumerate(prices) :
+            if i == len(prices) - 1 :
+                break
             
-            if max_profit < profit :
-                max_profit = profit
+            for j, price_j in enumerate(prices[i+1:]) :
+                profit = price_j - price_i
                 
+                if profit < 0 :
+                    break
+                elif max_profit < profit :
+                    max_profit = profit
+                    
         return max_profit
