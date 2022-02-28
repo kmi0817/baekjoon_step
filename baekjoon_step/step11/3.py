d781d4b300ed6a102536a716b19389a23d1bd82a
kmi0817 <kmi0817@naver.com>
2022-02-28 23:53:15 +0900

    still no clue

--- baekjoon_step/step11/3.py
+++ baekjoon_step/step11/3.py
@@ -9,19 +9,25 @@ for i, x in enumerate(data) :
   for y in data[i+1:] :
     if x[0] >= y[0] and x[1] >= y[1] :
       votes[i] += 1
+print(votes)
+print()
 
 ranks = [ 0 for i in range(n)]
 former_max = -1
 former_rank = -1
 for rank in range(1, n+1) :
-  max_elements = max(votes)
-  max_index = votes.index(max_elements)
+  max_element = max(votes)
+  max_index = votes.index(max_element)
+  votes.pop(max_index)
 
-  if max_elements == former_max :
+  if max_element == former_max :
     ranks[max_index] = former_rank
   else :
     ranks[max_index] = rank
-  former_max = max_elements
-  former_rank = max_index
+  print(f"current max: {max_element}({max_index}), rank: {ranks[max_index]} & foremr max: {former_max}({former_rank})")
+  print(votes)
+  print()
+  former_max = max_element
+  former_rank = rank
 
 print(f"ranks: {ranks}")
