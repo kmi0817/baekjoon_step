1ab9ebf284b669d7bcb9b09337fb36a75df9d7aa
kmi0817 <kmi0817@naver.com>
2022-03-04 17:01:08 +0900

    add more refined solution (using list & for loop)

--- Book/Greedy_ex.py
+++ Book/Greedy_ex.py
@@ -12,3 +12,13 @@ N %= 50
 min_cnt += N // 10
 
 print(f'최소 개수: {min_cnt}')
+
+# more refined solution
+N = int(input())
+
+coins = [500, 100, 50, 10]
+min_cnt = 0
+for coin in coins :
+    min_cnt += N // coin
+    N %= coin
+print(f'최소 개수: {min_cnt}')
