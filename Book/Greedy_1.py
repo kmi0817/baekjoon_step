ecc6644fb2c2d5b064dc1a8f908a42330f6757d8
kmi0817 <kmi0817@naver.com>
2022-03-05 23:34:00 +0900

    took 31 mins

---
+++ Book/Greedy_1.py
@@ -0,0 +1,16 @@
+N, M, K = map(int, input().split())
+Ns = list(map(int, input().split()))
+Ns.sort(reverse=True)
+
+sum_result = 0
+while M > 0 :
+    for k in range(K) :
+        if M == 0 : break
+
+        sum_result += Ns[0]
+        M -= 1
+    
+    sum_result += Ns[1]
+    M -= 1
+
+print(sum_result)
