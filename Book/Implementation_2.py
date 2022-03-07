3b93b58a31a1543ed95ba946b845acec06466704
kmi0817 <kmi0817@naver.com>
2022-03-07 23:34:55 +0900

    got idea from the book - took 28min to solve

--- Book/Implementation_2.py
+++ Book/Implementation_2.py
@@ -6,23 +6,35 @@ maps = []
 for _ in range(N) :
     maps.append(list(map(int, input().split())))
 
+# ordering: north, east, north, west
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
+turn_cnt = 0
 while True :
-    is_way = False
-    for _ in range(4) :
-        d -= 1
-        xx = x + dx[d%4]
-        yy = y + dy[d%4]
+    # turn left
+    d -= 1
+    xx = x + dx[d%4]
+    yy = y + dy[d%4]
 
-        if maps[xx][yy] == 0 and (xx, yy) not in steps :
-            is_way = True
-            steps.append((xx, yy))
+    # there's a way available
+    if maps[xx][yy] == 0 and (xx, yy) not in steps :
+        steps.append((xx, yy))
+        x, y = xx, yy
+        turn_cnt = 0
+        continue
+    else :
+        turn_cnt += 1
+    
+    # when it turns 4 times
+    if turn_cnt == 4 :
+        xx = x - dx[d%4]
+        yy = y - dy[d%4]
+        # check if back_step is available
+        if maps[xx][yy] == 0 :
             x, y = xx, yy
+            turn_cnt = 0
+        else :
             break
-
-    if is_way == False :
-        break
-
+    
 print(len(steps))
