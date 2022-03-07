3fe177d37f14951908ca337b823c0325388154db
kmi0817 <kmi0817@naver.com>
2022-03-07 09:52:42 +0900

    took 15mins (including previous - 29 mins)

--- Book/Implementation_1.py
+++ Book/Implementation_1.py
@@ -1,14 +1,20 @@
 starting_point = input()
-col = starting_point[0]
-row = int(starting_point[1])
 
-ways = 8
+cols = "abcdefgh"
+col = cols.index(starting_point[0]) + 1 # a
+row = int(starting_point[1]) # 1
 
-if col < "c" or col > "f" :
-    print(col)
-    ways -= 2
-if row < 3 or row > 6 :
-    print(row)
-    ways -= 2
+ways = [ ['l', 'l', 'u'], ['l', 'l', 'd'], ['r', 'r', 'u'], ['r', 'r', 'd'],
+            ['u', 'u', 'l'], ['u', 'u', 'r'], ['d', 'd', 'l'], ['d', 'd', 'r'] ]
 
-print(ways)
+cnt = 0
+for way in ways :
+    c, r = col, row
+    for d in way :
+        if d == 'l' : c -= 1
+        elif d == 'r' : c += 1
+        elif d == 'u' : r -= 1
+        elif d == 'd' : r += 1
+    if c >= 1 and c <= 8 and r >= 1 and r <= 8:
+        cnt += 1
+print(cnt)
