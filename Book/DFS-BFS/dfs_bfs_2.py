a744cfb6383072b4f913148080125d62b14ed026
박경미 <62174395+kmi0817@users.noreply.github.com>
2022-03-09 23:29:04 +0900

    remove: delete some code except appropriate code
    The former code included some wrong and meaningless code.
    So I deleted them all and let only well-functioned code remain: input N, M and graph

--- Book/DFS-BFS/dfs_bfs_2.py
+++ Book/DFS-BFS/dfs_bfs_2.py
@@ -1,25 +1,8 @@
-global N
-global M
+from collections import deque
+
 N, M = map(int, input().split())
 
-global step
-step = 0
-def dfs(graph, x, y) :
-    if x == N and y == M :
-        step += 1
-        return
-
-    if graph[x][y] == 1 and x <= N and y <= M :
-        graph[x][y] = 0 # 방문 처리
-        step += 1
-        dfs(graph, x, y+1)
-        dfs(graph, x+1, y)
-
 # graph
-graph = [[]]
+graph = []
 for n in range(N) :
     graph.append(list(map(int, input())))
-
-
-dfs(graph, 1, 1)
-print(step)
