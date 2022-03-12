1ed08a301dbb350917a82730be42fb4fde63c35e
kmi0817 <kmi0817@naver.com>
2022-03-12 23:33:52 +0900

    feat: :poop: print wrong answer because of 'step' variable
    I wrote it by referring to the textbook code. However, the wrong answer comes out because of the step variable. I don't know why, but compared to the correct answer in the textbook, I knew that the step variable was the cause.

--- Book/DFS-BFS/dfs_bfs_2.py
+++ Book/DFS-BFS/dfs_bfs_2.py
@@ -6,3 +6,31 @@ N, M = map(int, input().split())
 graph = []
 for n in range(N) :
     graph.append(list(map(int, input())))
+
+# up, down, left, right
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y) :
+    queue = deque([(x, y)])
+    step = 1
+    while queue :
+        x, y = queue.popleft()
+
+        for i in range(4) : # Check 4 directoins
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx <= -1 or nx >= N or ny <=-1 or ny >= M :
+                continue
+            
+            if graph[nx][ny] == 0 :
+                continue
+            
+            if graph[nx][ny] == 1 :
+                step += 1
+                graph[nx][ny] = step
+                queue.append((nx, ny))
+    return graph[N-1][M-1]
+
+print(bfs(0, 0))
