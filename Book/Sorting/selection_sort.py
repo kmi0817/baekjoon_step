8d2790b2d9549f4bf9e2ee12d41a51f0a94e4c0a
kmi0817 <kmi0817@naver.com>
2022-03-13 14:46:33 +0900

    feat: add more common solution
    Originally there was only a pythonic solution. I, however, wanted to make more common way solution of selection sort, so I made and added it.

--- Book/Sorting/selection_sort.py
+++ Book/Sorting/selection_sort.py
@@ -10,3 +10,18 @@ for i, n in enumerate(array) :
     array[i], array[j] = m, n # replace
 
 print(array)
+
+# more common way solution
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(len(array)) :
+    min_index = -1
+    min = 9999
+    for j in range(i+1, len(array)) :
+        if array[j] < min :
+            min = array[j]
+            min_index = j
+
+    array[i], array[min_index] = min, array[i]
+
+print(array)
