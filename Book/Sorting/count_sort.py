86086999ac49ba1b8c852a9e056f76c0b2c00970
kmi0817 <kmi0817@naver.com>
2022-03-14 00:01:40 +0900

    fix: change the length of 'count' list
    Originally, the length of 'count' list was the same as the array length. However, in counting sort, the length of 'count' list needs to be as long as the maximum value of 'array' list + 1, so it was modified.

--- Book/Sorting/count_sort.py
+++ Book/Sorting/count_sort.py
@@ -1,6 +1,6 @@
 array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
 
-count = [ 0 for _ in range(len(array))]
+count = [ 0 for _ in range(max(array) + 1)]
 for element in array :
     count[element] += 1
 
