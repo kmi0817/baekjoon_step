f9b8e71942144e265193463c265752e394d22853
kmi0817 <kmi0817@naver.com>
2022-03-16 15:13:00 +0900

    feat: add a recursive function binary_search
    the returns of recursive_binary_search() were confusing... It is hard for me to figure out when to state 'return' and when not to.

--- Book/Binary_Search/binary_search.py
+++ Book/Binary_Search/binary_search.py
@@ -14,7 +14,26 @@ def binary_search(array, target) :
     
     return "원소가 존재하지 않습니다"
 
+def recursive_binary_search(array, target, start, end) :
+    if start > end :
+        return None
+
+    middle = int((start + end) / 2)
+
+    if array[middle] == target :
+        return middle + 1
+    elif array[middle] < target :
+        return recursive_binary_search(array, target, middle+1, end)
+    else :
+        return recursive_binary_search(array, target, start, middle-1)
+
 n, target = map(int, input().split())
 array = list(map(int, input().split()))
 
-print(binary_search(array, target))
+# print(binary_search(array, target))
+
+result = recursive_binary_search(array, target, 0, n-1)
+if result == None :
+    print("원소가 존재하지 않습니다")
+else :
+    print(result)
