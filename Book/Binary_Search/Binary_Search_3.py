68cb299fe35e6af933ff46360762b73a1c410579
kmi0817 <kmi0817@naver.com>
2022-03-20 23:32:16 +0900

    fix: change results's range from 0 to max(array) + 1
    delete testing print() and change 'results' range to include max(array)

--- Book/Binary_Search/Binary_Search_3.py
+++ Book/Binary_Search/Binary_Search_3.py
@@ -5,24 +5,20 @@ n, m = map(int, input().split())
 array = list(map(int, input().split()))
 
 total_sum = -1
-results = [x for x in range(max(array))]
+results = [x for x in range(max(array)+1)]
 
 left = 0
 right = len(results)-1
 while left <= right :
     middle = (left + right) // 2
-    # print("middle", middle)
     subtracted_array = list(map(lambda x: x - results[middle] if x > results[middle] else 0, array)) # subtract results[middle] only if element is bigger than that
     total_sum = sum(subtracted_array)
-    # print(subtracted_array)
-    # print(total_sum)
+
     if total_sum == m :
         break
-
     elif total_sum > m :
         left = middle + 1
     else :
         right = middle - 1
 
-print(left, right)
 print(results[middle])
