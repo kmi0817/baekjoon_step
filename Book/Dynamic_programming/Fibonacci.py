a851feb6787b15ae6b27f58295e59dc13b088e21
kmi0817 <kmi0817@naver.com>
2022-03-21 11:01:08 +0900

    feat: add dynamic programming solution
    Reference: "이것이 취업을 위한 코딩 테스트다"

--- Book/Dynamic_programming/Fibonacci.py
+++ Book/Dynamic_programming/Fibonacci.py
@@ -1,3 +1,16 @@
+# Dynamic Programming
+def dynamic_fibo(n) :
+    if n <= 2 :
+        return 1
+
+    if array[n] != 0 :
+        return array[n]
+    else :
+        array[n] = dynamic_fibo(n-1) + dynamic_fibo(n-2)
+        return array[n]
+
+
+# O(2^N)
 def fibo(n) :
     if n <=2 :
         return 1
@@ -5,3 +18,6 @@ def fibo(n) :
     return fibo(n-1) + fibo(n-2)
 
 print(fibo(11))
+
+array = [0 for _ in range(100)]
+print(dynamic_fibo(99))
