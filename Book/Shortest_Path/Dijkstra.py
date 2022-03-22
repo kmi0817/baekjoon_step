887376d7cfea422935ae6e0b656c8454325819a5
kmi0817 <kmi0817@naver.com>
2022-03-22 16:22:21 +0900

    feat: :poop: cause an error
    unvisited_shortest_node() returns NoneType in dijkstra(), so in for loop in dijkstra() an error occured becuase list cannot be indiced by NoneType.

--- Book/Shortest_Path/Dijkstra.py
+++ Book/Shortest_Path/Dijkstra.py
@@ -1,3 +1,4 @@
+from operator import truediv
 import sys
 input = sys.stdin.readline
 INF = int(1e9)
@@ -8,10 +9,37 @@ start = int(input())
 graph = [[] for i in range(n+1)]
 visited = [ False for i in range(n+1)]
 distance = [ INF for i in range(n+1)]
+visited[start] = True
+distance[start] = 0
 
 # graph input
 for i in range(1, m+1) :
     v, u, dist = map(int, input().split())
     graph[v].append((u, dist))
 
-print(graph)
+def unvisited_shortest_node(visited, distance) :
+    temp = distance.copy()
+
+    path = min(temp)
+    v = temp.index(path)
+
+    if not visited[v] :
+        return v
+    else :
+        temp.pop(v)
+
+
+def dijkstra() :
+    for _ in range(n-1) :
+        v = unvisited_shortest_node(visited, distance)
+
+        for x in graph[v] :
+            u, d = x[0], x[1]
+            
+            if distance[v] + d < distance[u] :
+                distance[u] = distance[v] + d
+
+dijkstra() # excecute
+
+for d in distance[1:] :
+    print(d)
