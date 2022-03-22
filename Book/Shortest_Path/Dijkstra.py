f3c0f58e473539ee8e98955356e8182d93ae681d
kmi0817 <kmi0817@naver.com>
2022-03-22 16:29:01 +0900

    fix: :poop: executes well but output a wrong answer
    When I input as the same inputs in the book, it executed and made an result but the result was wrong. It showed 0, 1e9, 1e9, 1e0, 1e9, 1e9.

--- Book/Shortest_Path/Dijkstra.py
+++ Book/Shortest_Path/Dijkstra.py
@@ -17,21 +17,20 @@ for i in range(1, m+1) :
     v, u, dist = map(int, input().split())
     graph[v].append((u, dist))
 
-def unvisited_shortest_node(visited, distance) :
-    temp = distance.copy()
+def unvisited_shortest_node() :
+    min_path = distance[0]
+    index = 0
 
-    path = min(temp)
-    v = temp.index(path)
-
-    if not visited[v] :
-        return v
-    else :
-        temp.pop(v)
-
+    for i in range(1, n+1) :
+        if distance[i] < min_path and not visited[i] :
+            min_path = distance[i]
+            index = i
+    
+    return index
 
 def dijkstra() :
     for _ in range(n-1) :
-        v = unvisited_shortest_node(visited, distance)
+        v = unvisited_shortest_node()
 
         for x in graph[v] :
             u, d = x[0], x[1]
