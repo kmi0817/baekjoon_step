02901275fc9547a00312e353d459853a6ac0ff9a
kmi0817 <kmi0817@naver.com>
2022-04-09 23:59:51 +0900

    perf: :poop: cause TypeError
    Try to use recursive function but failed

--- Book/Graph/disjoint_sets.py
+++ Book/Graph/disjoint_sets.py
@@ -10,7 +10,17 @@ parents = [ i for i in range(v+1) ]
 for i in range(e) :
     a, b = map(int, input().split())
 
-    child = max(a, b)
+    child = max(parents[a], parents[b])
     parents[child] = min(parents[a], parents[b])
+
+# 재귀함수
+def find_root(node, root) :
+    if node == root :
+        return node
+    
+    find_root(root, parents[root])
+
+for n in range(1, v+1) :
+    parents[n] = find_root(n, parents[n])
 
 print(parents[1:])
