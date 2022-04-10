94028ec5169ddf64b5c875a1b883b99b2736b577
kmi0817 <kmi0817@naver.com>
2022-04-10 13:37:43 +0900

    fix: remove heapq and use joint_sets

--- Book/Graph/spanning_tree/kruskal.py
+++ Book/Graph/spanning_tree/kruskal.py
@@ -1,37 +1,29 @@
-import heapq
 import sys
 input = sys.stdin.readline
-
-v, e = map(int, input().split())
-parent = [ i for i in range(v+1) ]
-q = []
-
-# 간선 입력 받기
-for _ in range(e) :
-    x, y, d = map(int, input().split())
-    heapq.heappush(q, (d, (x, y)))
 
 def find_root(node) :
     if parent[node] != node :
         parent[node] = find_root(parent[node])
     return parent[node]
+def union(a, b) :
+    child = max(parent[a], parent[b])
+    parent[child] = find_root(min(parent[a], parent[b]))
 
-def kruskal() :
-    cost = 0
+v, e = map(int, input().split())
+parent = [ i for i in range(v+1) ]
+edges = []
 
-    while q :
-        c, pair = heapq.heappop(q)
-        child = min(parent[pair[0]], parent[pair[1]])
-        parent[child] = find_root(max(parent[pair[0]], parent[pair[1]]))
-        if parent[pair[0]] != parent[pair[1]] :
-            cost += c
-            print(pair, cost)
-            
-            parent[pair[0]] = -1
-            parent[pair[1]] = -1
+# 간선 정보 입력
+for _ in range(e) :
+    x, y, c = map(int, input().split())
+    edges.append((c, x, y))
+edges.sort() # 정렬
 
-    return cost
+cost = 0
+for edge in edges :
+    c, x, y = edge
+    if parent[x] != parent[y] :
+        cost += c
+        union(x, y)
 
-# Kruskal 실행
-minimum_cost = kruskal()
-print(minimum_cost)
+print(cost)
