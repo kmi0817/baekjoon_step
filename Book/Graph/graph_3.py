304d54fa3bf4e199c14503d6b07cee89ad5a33b7
kmi0817 <kmi0817@naver.com>
2022-04-12 17:27:14 +0900

    feat: :poop: make a wrong result

---
+++ Book/Graph/graph_3.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+def find_root(node) :
+    if parent[node] != node :
+        parent[node] = find_root(parent[node])
+    return parent[node]
+
+def union(a, b) :
+    child = max(parent[a], parent[b])
+    parent[child] = find_root(min(parent[a], parent[b]))
+
+n, m = map(int, input().split())
+parent = [ i for i in range(n+1) ]
+edges = []
+
+cost = []
+for _ in range(m) :
+    a, b, d = map(int, input().split())
+
+    if parent[a] != parent[b] :
+        union(a, b)
+        cost.append(d)
+    
+print(sum(cost) - max(cost))
