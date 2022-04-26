cec43271988bf137b41fea0a75c215f29c32dd49
kmi0817 <kmi0817@naver.com>
2022-04-26 21:41:51 +0900

    chore: :bulb: add a moduler of "modular operation"

--- baekjoon_step/1924.py
+++ baekjoon_step/1924.py
@@ -9,5 +9,5 @@ total_days = d # 입력 월의 days에
 for n in MONTH[:m-1] :
     total_days += n # 입력 월 이전 days (=n) 더하기
 
-index = total_days % 7 - 1
+index = total_days % 7 - 1 # 전체 days를 요일의 개수 7로 모듈러 연산
 print(DAYS[index])
