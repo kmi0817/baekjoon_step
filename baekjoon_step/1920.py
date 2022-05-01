3cda06503ac33ddbdc72edef48e2cbeb803e7a9a
kmi0817 <kmi0817@naver.com>
2022-05-01 23:08:02 +0900

    fix: use Binary Search
    It was Timeout but now it succeeded with 860ms.

--- baekjoon_step/1920.py
+++ baekjoon_step/1920.py
@@ -1,11 +1,27 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 ns = list(map(int, input().split()))
+ns.sort()
 
 m = int(input())
 ms = list(map(int, input().split()))
 
 for x in ms :
-    if x in ns :
+    start, end = 0, n - 1
+    is_there = False
+    while start <= end :
+        i = (start + end) // 2
+        if x == ns[i] :
+            is_there = True
+            break
+        elif x < ns[i] :
+            end = i - 1
+        else :
+            start = i + 1
+        
+    if is_there :
         print(1)
     else :
         print(0)
