3528ec0ced65bf9d7ffafbf1d828430ccdaa823e
kmi0817 <kmi0817@naver.com>
2022-05-09 16:09:50 +0900

    feat: add a solution created during review

--- Book/Greedy/Greedy_3.py
+++ Book/Greedy/Greedy_3.py
@@ -1,4 +1,19 @@
-# minus -1 or divide by K until N becomes 1
+# 2022-05-09
+n, k = map(int, input().split())
+
+answer = 0
+while n > 1:
+    if n % k != 0:
+        n -= 1
+        answer += 1
+
+    else:
+        n //= k
+        answer += 1
+
+print(answer)
+
+# 2022-03-08 minus -1 or divide by K until N becomes 1
 
 N, K = map(int, input().split())
 
