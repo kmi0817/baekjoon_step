68ff624f0a0576cbbca6a05e9f91a03e3d3e21e5
kmi0817 <kmi0817@naver.com>
2022-05-09 15:55:32 +0900

    feat: add a solution created during review

--- Book/Greedy/Greedy_2.py
+++ Book/Greedy/Greedy_2.py
@@ -1,3 +1,14 @@
+# 2022-05-09
+n, m = map(int, input().split())
+
+smallest_card = []  # 각 행마다 가장 작은 값이 저장될 리스트
+for _ in range(n):
+    smallest_card.append(min(list(map(int, input().split()))))
+    # 입력받은 한 행 (문자열)을 리스트로 전환한 후 최소 값을 반환하는 min() 함수 사용
+
+print(max(smallest_card)) # 각 행에서 가장 작은 값들을 모은 리스트에서 가장 큰 값을 출력
+
+# 2022-03-08
 N, M = map(int, input().split())
 
 _max = -1
