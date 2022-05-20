b8321f6480b3de8bfe2c45ece06f2d78f05f4e74
kmi0817 <kmi0817@naver.com>
2022-05-20 21:50:16 +0900

    chore: 모르겠음

--- Book/Chapter11/Q01.py
+++ Book/Chapter11/Q01.py
@@ -1,25 +1,9 @@
+from audioop import reverse
 from collections import deque
 
-n = int(input())  # 모험가 수
-chart = list(map(int, input().split()))  # 공포도
+n = int(input())
+graph = deque(list(map(int, input().split())))
 
-chart.sort()  # 오름차순 정렬
-q = deque(chart)  # 큐
+graph.sort(reverse=True) # 공포도 내림차순 정렬
 
-answer = 0
-while q:
-    value = q[0]  # 가장 낮은 공포도
-
-    if len(q) >= value:
-        print(q)
-        print(f'value before popleft() : {value}')
-        for i in range(value):
-            q.popleft()
-        print()
-        print(f'value after popleft() : {value}')
-
-        answer += 1
-    else:
-        break
-
-print(answer)
+group = 0
