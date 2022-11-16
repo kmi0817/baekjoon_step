ec598d7a28cda75d29d61bb214ca50c1bef9efbf
kmi0817 <kmi0817@naver.com>
2022-11-16 18:08:25 +0900

    perf: :poop: 틀린 코드
    정확성 60.0 / 100.0

--- Programmers/코딩테스트_연습_힌트_모음집/체육복.py
+++ Programmers/코딩테스트_연습_힌트_모음집/체육복.py
@@ -1,23 +1,19 @@
 def solution(n, lost, reserve):
-    std = [1] * (n+1) # 도난 당하기 전 상태
-    for n in lost : # 도난 당한 학생
-        std[n] -= 1
-    for n in reserve : # 여벌 체육복 소유 학생
-        std[n] += 1
+    std = [1] * n # 도난 당하기 전 상태
+    for i in lost : # 도난 당한 학생
+        std[i-1] -= 1
+    for i in reserve : # 여벌 체육복 소유 학생
+        std[i-1] += 1
     # 0 : 체육복 없음, 1 : 여벌 체육복X, 2 : 여벌 체육복O
     
-    for i in range(1, n+2) :
-        if i == 1 and std[i] == 0 and std[i+1] == 2 :
-            std[i], std[i+1] = 1, 1
-            
-        elif i == (n+1) and std[i] == 0 and std[i-1] == 2 :
-            std[i-1], std[i] = 1, 1
-            
-        else :
-            if std[i] == 0 and std[i-1] == 2 :
+    for i in range(n) :
+        if i > 0 :
+            if std[i-1] == 0 and std[i] == 2 :
                 std[i-1], std[i] = 1, 1
-            elif std[i] == 0 and std[i+1] == 2 :
-                std[i], std[i+1] = 1, 1            
+        
+        if i < n-1 :
+            if std[i] == 0 and std[i+1] == 2 :
+                std[i], std[i+1] = 1, 1
             
-    answer = len(std[1:]) - std[1:].count(0)
+    answer = len(std) - std.count(0)
     return answer
