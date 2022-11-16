d19a5b3280bd50a12eee918857e8c5e041e4298a
kmi0817 <kmi0817@naver.com>
2022-11-16 18:30:51 +0900

    perf: 집합 사용한 정답 코드

--- Programmers/코딩테스트_연습_힌트_모음집/체육복.py
+++ Programmers/코딩테스트_연습_힌트_모음집/체육복.py
@@ -1,19 +1,15 @@
 def solution(n, lost, reserve):
-    std = [1] * n # 도난 당하기 전 상태
-    for i in lost : # 도난 당한 학생
-        std[i-1] -= 1
-    for i in reserve : # 여벌 체육복 소유 학생
-        std[i-1] += 1
-    # 0 : 체육복 없음, 1 : 여벌 체육복X, 2 : 여벌 체육복O
+    n_lost = list(set(lost) - set(reserve))
+    n_reserve = list(set(reserve) - set(lost))
     
-    for i in range(n) :
-        if i > 0 :
-            if std[i-1] == 0 and std[i] == 2 :
-                std[i-1], std[i] = 1, 1
-        
-        if i < n-1 :
-            if std[i] == 0 and std[i+1] == 2 :
-                std[i], std[i+1] = 1, 1
+    borrow = set()
+    for x in n_lost :
+        if x-1 in n_reserve :
+            borrow.add(x)
+            n_reserve.remove(x-1)
+        elif x+1 in n_reserve :
+            borrow.add(x)
+            n_reserve.remove(x+1)
             
-    answer = len(std) - std.count(0)
+    answer = n - (len(n_lost) - len(borrow))
     return answer
