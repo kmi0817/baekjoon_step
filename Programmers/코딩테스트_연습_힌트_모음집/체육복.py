5a3346d99bd8708aa8911907bcb2990bace2891f
kmi0817 <kmi0817@naver.com>
2022-11-16 17:50:27 +0900

    perf: :poop: 틀린 코드
    정확성 55.0 / 100.0

--- Programmers/코딩테스트_연습_힌트_모음집/체육복.py
+++ Programmers/코딩테스트_연습_힌트_모음집/체육복.py
@@ -5,13 +5,19 @@ def solution(n, lost, reserve):
     for n in reserve : # 여벌 체육복 소유 학생
         std[n] += 1
     # 0 : 체육복 없음, 1 : 여벌 체육복X, 2 : 여벌 체육복O
-    print(std)
     
-    for index, value in enumerate(std[:n]) :
-        if index == 0 : continue # 인덱스 0은 건너뜀
-        
-        if value == 0 and std[index+1] == 2 : # 뒷사람이 앞사람한테 빌려줌
-            std[index], std[index+1] = 1, 1
+    for i in range(1, n+2) :
+        if i == 1 and std[i] == 0 and std[i+1] == 2 :
+            std[i], std[i+1] = 1, 1
+            
+        elif i == (n+1) and std[i] == 0 and std[i-1] == 2 :
+            std[i-1], std[i] = 1, 1
+            
+        else :
+            if std[i] == 0 and std[i-1] == 2 :
+                std[i-1], std[i] = 1, 1
+            elif std[i] == 0 and std[i+1] == 2 :
+                std[i], std[i+1] = 1, 1            
             
     answer = len(std[1:]) - std[1:].count(0)
     return answer
